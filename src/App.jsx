7dcbd811d2c2159f09477e2260d0267589e12bf6
Yashchodavadiya <yyashchodavadiya@gmail.com>
2024-12-11 11:16:25 +0530

    REDUX

--- src/App.jsx
+++ src/App.jsx
@@ -7,45 +7,46 @@ import Crud from './Crud'
 import Form from '../src/Form'
 import Comment from './Comment'
 import Arrayform from './Arrayform'
+import AddData from './REDUX/AddData'
 
 function App() {
 
 
- {/* ================Props=================== */}
-  
- const [sum,setSum] = useState()
- const Lax = () => {
-    let a=5,b=5,ans;
-   ans = a+b;
+  {/* ================Props=================== */ }
+
+  const [sum, setSum] = useState()
+  const Lax = () => {
+    let a = 5, b = 5, ans;
+    ans = a + b;
     setSum(ans);
     console.log(ans);
-}
+  }
 
 
 
-const student={
-  name:"Lax",
-  age: 19,
-  city : "Ahmedabad"
-}
+  const student = {
+    name: "Lax",
+    age: 19,
+    city: "Ahmedabad"
+  }
 
-const arr = [1,"Lax",2,"om"]
+  const arr = [1, "Lax", 2, "om"]
 
 
-{/* ==============counter-using Prop================== */}
+  {/* ==============counter-using Prop================== */ }
 
-const [val,setVal] = useState(0)
-const func = () => {
- 
-const sum = val+1
- setVal(sum)
- 
- console.log(val);
-}
+  const [val, setVal] = useState(0)
+  const func = () => {
+
+    const sum = val + 1
+    setVal(sum)
+
+    console.log(val);
+  }
 
   return (
     <>
-     {/* PROMP
+      {/* PROMP
     <br />
      <Prop 
            newans={sum}
@@ -56,12 +57,13 @@ const sum = val+1
           array={arr}
           studentdata={student}
           /> */}
-    {/* <Counter /> */}
-    {/* <Spreading /> */}
-    <Crud />
-    {/* <Form /> */}
-    {/* <Arrayform /> */}
-    {/* <Comment /> */}
+      {/* <Counter /> */}
+      {/* <Spreading /> */}
+      {/* <Crud /> */}
+      {/* <Form /> */}
+      {/* <Arrayform /> */}
+      {/* <Comment /> */}
+      <AddData />
     </>
   )
 }
